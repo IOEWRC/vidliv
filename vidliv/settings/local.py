b850c7225778ae23e7214dd99d5736a46b73f42a
yathomasi <yathomasi@gmail.com>
2018-05-28 23:10:16 +0545

    managed environment file

--- vidliv/settings/local.py
+++ vidliv/settings/local.py
@@ -1,21 +1,20 @@
 import os
 
-from decouple import config
+from decouple import config, Csv
 
 from django.conf import settings
 
-DEBUG = True
-ALLOWED_HOSTS = []
-
+DEBUG = config('DEBUG', default=True, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 DATABASES = {  # set ENV `DATABASE_NAME' to `postgresql' to use otherwise it will use default sqllite3
     # replace with your own database credential
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'vidliv',
-        'USER': 'forvidliv',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': '5432',
     } if config('DATABASE_NAME') == 'postgresql' else {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -28,3 +27,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # for media files
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+SECRET_KEY = config('SECRET_KEY')
