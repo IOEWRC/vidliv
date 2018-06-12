c828a872398a85532380e5043cfe348cc6c55757
Sagar Sapkota <spkt.sagar@gmail.com>
2018-06-12 23:22:37 +0545

    Simple one to one

--- test_home_app/urls.py
+++ test_home_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'test_home_app'
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    # path('', views.home, name="home"),
+    path('', views.UserListView.as_view(), name='home'),
 ]
--- test_home_app/views.py
+++ test_home_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.models import User
 
 
 # Create your views here.
-def home(request):
-    return render(request, 'test_home_app/home.html')
+# def home(request):
+#     return render(request, 'test_home_app/home.html')
+
+
+class UserListView(ListView):
+    model = User
+    context_object_name = 'user_list'
+    template_name = 'test_home_app/home.html'
