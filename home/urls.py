0646e6d529a2852642af04000bb4478e0638ce0f
Sagar Sapkota <spkt.sagar@gmail.com>
2018-07-05 11:33:12 +0545

    Simple multibroadcast django vieww, ui remaining

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,6 @@ app_name = 'home'
 urlpatterns = [
     # path('', views.home, name="home"),
     path('', views.UserListView.as_view(), name='home'),
+    path('multibroadcast/', views.multi_broadcast, name='gomultibroadcast'),
+    path('multibroadcast/<str:action>/<str:username>', views.multi_broadcast, name='multistreamaction'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.contrib.auth.models import User
@@ -15,3 +16,22 @@ class UserListView(ListView):
 
     def get_queryset(self):
         return User.objects.exclude(username=self.request.user.username)
+
+
+def multi_broadcast(request, action=None, username=None):
+    if not username:
+        return render(request, 'home/multi_broadcaster.html', {
+            'broadcaster': True,
+            'roomid': request.user.username + '-stream_room',
+        })
+    elif username and action == 'view':
+        return render(request, 'home/multi_broadcaster.html', {
+            'broadcaster': False,
+            'roomid': username + '-stream_room',
+        })
+    elif username and action == 'join':
+        return render(request, 'home/multi_broadcaster.html', {
+            'broadcaster': True,
+            'roomid': username + '-stream_room',
+        })
+    raise Http404
